32c5b7eaa4c63629b41f891f93d700d666899df6
sai samant <saisamant@gmail.com>
2017-07-07 12:14:28 -0700

    added season and location models, added data to users

--- farm_site/models.py
+++ farm_site/models.py
@@ -9,8 +9,23 @@ class Member(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     zipcode = models.CharField(max_length=5)
+    phone = models.CharField(max_length=12, default='206-555-5555')
     created_date = models.DateTimeField(
             default=timezone.now)
+    # seasons = models.ManyToManyField(Season)
+    # locations = models.ManyToManyField(Location)
 
     def __str__(self):
         return self.last_name
+
+class Season(models.Model):
+    year = models.CharField(max_length=4)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=5)
+    current = models.BooleanField
--- farm_site/views.py
+++ farm_site/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Member
 
 def index(request):
     return render(request, 'farm_site/index.html', {})
+
+def members(request):
+    members = Member.objects.all
+    return render(request, 'farm_site/members.html', {'members': members})
+
+
+    
