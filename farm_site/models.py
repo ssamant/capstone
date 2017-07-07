cdc2c92c92e42a391bb503e127c8d9a6aa9d23c2
sai samant <saisamant@gmail.com>
2017-07-07 15:54:43 -0700

    added signup to model and made it into join table

--- farm_site/models.py
+++ farm_site/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 from django.utils import timezone
 
+class Season(models.Model):
+    year = models.CharField(max_length=4)
+
+    def __str__(self): return self.year
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=5)
+    current = models.BooleanField
+
+    def __str__(self): return self.name
+
+
 class Member(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -18,14 +34,10 @@ class Member(models.Model):
     def __str__(self):
         return self.last_name
 
-class Season(models.Model):
-    year = models.CharField(max_length=4)
+class Signup(models.Model):
+    member = models.ForeignKey(Member)
+    location = models.ForeignKey(Location)
+    season = models.ForeignKey(Season)
 
-
-class Location(models.Model):
-    name = models.CharField(max_length=50)
-    street_address = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=2)
-    zipcode = models.CharField(max_length=5)
-    current = models.BooleanField
+    def __str__(self):
+        return "Name: %s, Year: %s" % (self.member, self.season)
