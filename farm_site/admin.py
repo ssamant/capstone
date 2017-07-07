5ed5f06d5f6f8d9a11376034d1e1e7a545f8bafb
sai samant <saisamant@gmail.com>
2017-07-07 12:17:28 -0700

    added location and season to admin

--- farm_site/admin.py
+++ farm_site/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Member
+from .models import Location
+from .models import Season
 
 
 admin.site.register(Member)
+admin.site.register(Location)
+admin.site.register(Season)
