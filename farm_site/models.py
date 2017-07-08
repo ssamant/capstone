c249a6d814063c626661e66daaef15adf2da8b9c
sai samant <saisamant@gmail.com>
2017-07-08 00:13:45 -0700

    updated fixtures

--- farm_site/models.py
+++ farm_site/models.py
@@ -12,7 +12,7 @@ class Location(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
     zipcode = models.CharField(max_length=5)
-    current = models.BooleanField
+    current = models.BooleanField()
 
     def __str__(self): return self.name
 
@@ -28,8 +28,6 @@ class Member(models.Model):
     phone = models.CharField(max_length=12, default='206-555-5555')
     created_date = models.DateTimeField(
             default=timezone.now)
-    # seasons = models.ManyToManyField(Season)
-    # locations = models.ManyToManyField(Location)
 
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
