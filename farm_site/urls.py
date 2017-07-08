489b0df316eb6b48fe2ceb14aed2ed3286a535fe
sai samant <saisamant@gmail.com>
2017-07-08 10:40:01 -0700

    views for restaurants and csa sign ups

--- farm_site/urls.py
+++ farm_site/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'csa', views.csa, name='csa'),
+    url(r'restaurants', views.restaurants, name='restaurants'),
     url(r'dashboard/members', views.members, name='members'),
     url(r'dashboard/signups', views.signups, name='signups'),
     url(r'dashboard/locations', views.locations, name='locations'),
--- farm_site/views.py
+++ farm_site/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, render, get_object_or_404, redirect
 from .models import Member
 from .models import Location
 from .models import Signup
+from .models import Season
 
+# general views
 def index(request):
     return render(request, 'farm_site/index.html', {})
+
+def csa(request):
+    return render(request, 'farm_site/csa.html', {})
+
+def restaurants(request):
+    return render(request, 'farm_site/restaurants.html', {})
+
+# csa member views
+
+
+# farmers views
+def dashboard(request):
+    return render(request, 'farm_site/dashboard.html', {})
+
 
 def members(request):
     members = Member.objects.all
     return render(request, 'farm_site/members.html', {'members': members})
-
-
-def dashboard(request):
-    return render(request, 'farm_site/dashboard.html', {})
-
 
 def locations(request):
     locations = Location.objects.filter(current=True)
