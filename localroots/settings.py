330bb8d4a5881c10c34bf704bc3db9d989b319ec
sai samant <saisamant@gmail.com>
2017-07-22 14:36:13 -0700

    changing config and db settings for eb deploy

--- localroots/settings.py
+++ localroots/settings.py
@@ -77,16 +77,28 @@ WSGI_APPLICATION = 'localroots.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farmsite',
-        'USER': 'sai',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+            }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'farmsite',
+            'USER': 'sai',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 
 
 AUTH_USER_MODEL = 'farm_site.User'
