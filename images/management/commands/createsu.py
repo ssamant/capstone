00ba59691a39cf67628715ea8d23b7a281e972eb
sai samant <saisamant@gmail.com>
2017-07-23 11:06:24 -0700

    create superuser command added

--- images/management/commands/createsu.py
+++ images/management/commands/createsu.py
@@ -7,3 +7,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
             User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            self.stdout.write(self.style.SUCCESS('Successfully created new superuser'))
