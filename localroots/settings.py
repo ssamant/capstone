cef13cf19e13879b8ad6e748f6b5dafb428ab896
sai samant <saisamant@gmail.com>
2017-07-23 10:30:51 -0700

    added import logging to settings.py

--- localroots/settings.py
+++ localroots/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import logging
 from django.core.exceptions import ImproperlyConfigured
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
