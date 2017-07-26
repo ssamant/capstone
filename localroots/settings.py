355f25d24fca470ca5c1b1a3ab17d4316fb1a400
sai samant <saisamant@gmail.com>
2017-07-26 08:01:18 -0700

    added maps key

--- localroots/settings.py
+++ localroots/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+GOOGLE_MAPS_KEY = os.environ.get('GOOGLE_MAPS_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
